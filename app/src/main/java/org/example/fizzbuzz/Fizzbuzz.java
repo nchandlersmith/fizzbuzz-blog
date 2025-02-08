afd758c7299158624f9525e94fa560f3682ebd18
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2025-02-08 08:13:46 -0600

    Refactor: return input as string

--- app/src/main/java/org/example/fizzbuzz/Fizzbuzz.java
+++ app/src/main/java/org/example/fizzbuzz/Fizzbuzz.java
@@ -2,9 +2,6 @@ package org.example.fizzbuzz;
 
 public class Fizzbuzz {
     public String run(Integer input) {
-        if (2 == input) {
-            return "2";
-        }
-        return "1";
+        return String.valueOf(input);
     }
 }
